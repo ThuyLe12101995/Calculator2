d3494fcc7adf63feec3233d9ae47aba72b1649cb
ThuyLe12101995 <thuy.leminh31@gmail.com>
2018-02-04 21:47:22 +0700

    Add Class calculator 2

---
+++ Calculator2.js
@@ -0,0 +1,20 @@
+const operators= require("./Operators");
+
+
+class Calculator {
+
+    constructor() {
+        this.operators = {
+            "+":new operators.Add(),
+            '-':new operators.Sub(),
+            '*':new operators.Mul(),
+            '/':new operators.Div()
+        };
+    }
+
+    calculate (operator,firstNumber,secondNumber) {
+        return this.operators[operator].run(firstNumber,secondNumber);
+    }
+}
+
+module.exports=Calculator;
